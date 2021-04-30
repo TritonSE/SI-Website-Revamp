0ac2f684d5303f03bc052e2d5f11ed51bc365543
PatrickBrown1 <patrickbrown1312@gmail.com>
2021-04-29 20:16:52 -0700

    custom arrows

--- client/website/src/components/Main/Slideshow.js
+++ client/website/src/components/Main/Slideshow.js
@@ -5,14 +5,44 @@
     Fade effect, pause on hover, auto-play, delay 3000 per slide, change color of location indicators 
 */
 import React from "react";
+import 'react-slideshow-image/dist/styles.css'
+import { Fade } from 'react-slideshow-image';
+import LeftArrow from '../../media/leftarrow.svg';
+import RightArrow from '../../media/rightarrow.svg';
+import "../../css/Slideshow.css";
 
-const Slideshow = (props) => {
-    const { SlideList } = props;
-
-    return(
+const renderPrevArrow = (e) => {
+    console.log(e);
+    return (
         <div>
-            
+            <img className="Slideshow_arrow" src={LeftArrow} alt="left arrow"/>
         </div>
+    )
+}
+const renderNextArrow = (e) => {
+    console.log(e);
+    return (
+        <div>
+            <img className="Slideshow_arrow" src={RightArrow} alt="right arrow"/>
+        </div>
+    )
+}
+const Slideshow = (props) => {
+    return(
+        <span className="carousel">
+        <Fade 
+            // transitionTime={3000}
+            autoPlay={true}
+            duration={3000}
+            transitionDuration={500}
+            prevArrow={<div style={{width: "50px", marginRight: "-50px"}}><img className="Slideshow_arrowleft" src={LeftArrow} alt="left arrow"/></div>}
+            nextArrow={<div style={{width: "50px", marginLeft: "-100px"}}><img className="Slideshow_arrowright" src={RightArrow} alt="right arrow"/></div>}
+
+            indicators={true}
+        >
+            {props.children}
+        </Fade>
+        </span>
     );
 };
 export default Slideshow;
--- client/website/src/pages/Resources/EPublications.js
+++ client/website/src/pages/Resources/EPublications.js
@@ -4,7 +4,16 @@ export default function EPublications() {
     return(
         <div>
             <p> This is the E-Publications Page </p>
-            <Slideshow SlideList={[]}/>
+            <Slideshow>
+                <div>
+                    <img style={{minWidth: "1500px", maxHeight: "300px"}} alt="cat" src="https://api.timeforkids.com/wp-content/uploads/2020/08/animalVoting.jpg?w=1024" />
+                    <p>meerkats :)</p>
+                </div>
+                <div>
+                    <img style={{maxWidth: "600px", maxHeight: "300px"}} alt="cat" src="https://www.trafalgar.com/real-word/wp-content/uploads/sites/3/2019/10/giant-panda-750x400.jpg" />
+                    <p>panda :O</p>
+                </div>
+            </Slideshow>
         </div>
     );
 };
