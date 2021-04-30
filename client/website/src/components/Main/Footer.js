4f24cd0594638373e5db753478b3deb54277442f
Amrit-S <aksingh@ucsd.edu>
2021-04-30 02:19:23 -0700

    started working on footer

--- client/website/src/components/Main/Footer.js
+++ client/website/src/components/Main/Footer.js
@@ -1,9 +1,33 @@
 import React from "react";
+import {SITE_PAGES} from "../../constants/links";
+import "../../css/Footer.css";
+
+import {AiOutlineFacebook, AiOutlineInstagram} from "react-icons/ai";
 
 export default function Footer() {
     return(
-        <div style={{backgroundColor: '#8477B9', height: "3vw", textAlign: "center"}}>
-            <p> Footer </p>
+        <div className="Footer-Main-Container">
+            <section className="Footer-Pages">
+                <a href={SITE_PAGES.HOME}> Home </a>
+                <a href={SITE_PAGES.HOME}> Conferences </a>
+                <a href={SITE_PAGES.HOME}> Resouces </a>
+                <a href={SITE_PAGES.HOME}> About Us </a>
+                <a href={SITE_PAGES.HOME}> Contact Us </a>
+            </section>
+            <section className="Footer-Bottom">
+                <section className="Footer-Social-Media">
+                    <a href="https://material-ui.com/components/icons/">
+                        <AiOutlineFacebook 
+                        style={{color: "white"}}
+                        // onMouseOver={({target})=>target.style.color='red'} 
+                        // onMouseOut={({target})=>target.style.color='white'}
+                        />
+                    </a>
+                    <AiOutlineInstagram style={{color: "white"}}/>
+                </section>
+                <p> logo </p>
+
+            </section>
         </div>
     );
 };
