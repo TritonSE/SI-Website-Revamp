31c6a69800cc3a88e3ad25ed7e8fdd65499ff84e
PatrickBrown1 <patrickbrown1312@gmail.com>
2021-04-29 18:56:18 -0700

    initial slideshow component file creation

---
+++ client/website/src/components/Main/Slideshow.js
@@ -0,0 +1,18 @@
+/*
+    Special Feature: Slideshow
+    Make this into a reusable component that takes in props (list of slides)!
+    Home, EPubs, and Conferences should have custom Slide components that they feed into this slideshow
+    Fade effect, pause on hover, auto-play, delay 3000 per slide, change color of location indicators 
+*/
+import React from "react";
+
+const Slideshow = (props) => {
+    const { SlideList } = props;
+
+    return(
+        <div>
+            
+        </div>
+    );
+};
+export default Slideshow;
--- client/website/src/pages/Resources/EPublications.js
+++ client/website/src/pages/Resources/EPublications.js
@@ -1,9 +1,10 @@
 import React from "react";
-
+import Slideshow from "../../components/Main/Slideshow.js"
 export default function EPublications() {
     return(
         <div>
             <p> This is the E-Publications Page </p>
+            <Slideshow SlideList={[]}/>
         </div>
     );
 };
