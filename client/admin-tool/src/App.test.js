63e11f8d50479d7c4cc170ebdbf3581ce9b7fb41
Thomas Garry <32282600+thomas-gg@users.noreply.github.com>
2021-05-01 20:54:07 -0700

    undo test

--- client/admin-tool/src/App.test.js
+++ client/admin-tool/src/App.test.js
@@ -4,6 +4,5 @@ import App from "./App";
 test("renders learn react link", () => {
     render(<App />);
     const linkElement = screen.getByText(/learn react/i);
-    if (1 == 1) console.log("yes");
     expect(linkElement).toBeInTheDocument();
 });
