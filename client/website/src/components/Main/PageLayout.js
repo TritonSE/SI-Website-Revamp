aba6d84cf6a14a7e6f2b6d752d11890acb61cfec
Amrit-S <aksingh@ucsd.edu>
2021-05-03 17:51:09 -0700

    added constants for color palette + fonts

--- client/website/src/components/Main/PageLayout.js
+++ client/website/src/components/Main/PageLayout.js
@@ -5,10 +5,13 @@ import Footer from "./Footer";
 
 export default function PageLayout(props) {
     return(
-        <div>
-           <NavBar/>
-            {props.children}
+        <body>
+            {/* minheight ensures footer is always at the bottom of the page */}
+            <div style={{ minHeight: "calc(100vh - 375px)"}}>
+                <NavBar/>
+                {props.children}
+            </div>
             <Footer/>
-        </div>
+        </body>
     );
 };
