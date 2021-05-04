edd4dddbbb4f6447ed38c81694937847854be859
Amrit-S <aksingh@ucsd.edu>
2021-05-03 21:30:50 -0700

    made mobile responsive

--- client/website/src/components/Main/Footer.js
+++ client/website/src/components/Main/Footer.js
@@ -7,57 +7,136 @@
  *
  * @author Amrit Kaur Singh
  */
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { AiOutlineFacebook, AiOutlineInstagram } from "react-icons/ai";
 import Brand from "./Brand";
 import { SITE_PAGES } from "../../constants/links";
 import "../../css/Footer.css";
 
 export default function Footer() {
-    return (
-        // outer relative div is needed so navbar can function properly
-        <div style={{position: "relative"}}>
-            <div className="Footer-Main-Container">
-                {/* all linked pages in website  */}
-                <section className="Footer-Pages">
-                    <a href={SITE_PAGES.HOME}> Home </a>
-                    <a href={SITE_PAGES.CONFERENCES}> Conferences </a>
-                    <a href={SITE_PAGES.RESOURCES_LANDING}> Resouces </a>
-                    <a href={SITE_PAGES.ABOUT_US}> About Us </a>
-                    <a href={SITE_PAGES.CONTACT_US}> Contact Us </a>
-                </section>
-                <section className="Footer-Bottom">
-                    {/* social media icons + links */}
-                    <section className="Footer-Social-Media">
-                        {/* facebook */}
-                        <a href="https://www.facebook.com/">
-                            <AiOutlineFacebook
-                                style={{ color: "white", borderRadius: "100px" }}
-                                onMouseOut={({ target }) => {
-                                    target.style.color = "white";
-                                }}
-                                onMouseOver={({ target }) => {
-                                    target.style.color = "#EA8644";
-                                }}
-                            />
-                        </a>
-                        {/* instagram */}
-                        <a href="https://www.instagram.com/">
-                            <AiOutlineInstagram
-                                style={{ color: "white" }}
-                                onMouseOut={({ target }) => {
-                                    target.style.color = "white";
-                                }}
-                                onMouseOver={({ target }) => {
-                                    target.style.color = "#EA8644";
-                                }}
-                            />
-                        </a>
+
+    const MAX_MOBILE_VIEW_WIDTH = 750;
+
+    const [windowSize, setWindowSize] = useState({
+        width: undefined
+    });
+
+      useEffect(() => {
+        // Handler to call on window resize
+        function handleResize() {
+          // Set window width/height to state
+          setWindowSize({
+            width: window.innerWidth
+          });
+        }
+        // Add event listener
+        window.addEventListener("resize", handleResize);
+        // Call handler right away so state gets updated with initial window size
+        handleResize();
+        // Remove event listener on cleanup
+        return () => window.removeEventListener("resize", handleResize);
+      }, []); // Empty array ensures that effect is only run on mount
+
+    if(windowSize.width > MAX_MOBILE_VIEW_WIDTH){
+        
+        return (
+            // outer relative div is needed so navbar can function properly
+            <div style={{position: "relative"}}>
+                <div className="Footer-Main-Container">
+                    {/* all linked pages in website  */}
+                    <section className="Footer-Pages">
+                        <a href={SITE_PAGES.HOME}> Home </a>
+                        <a href={SITE_PAGES.CONFERENCES}> Conferences </a>
+                        <a href={SITE_PAGES.RESOURCES_LANDING}> Resouces </a>
+                        <a href={SITE_PAGES.ABOUT_US}> About Us </a>
+                        <a href={SITE_PAGES.CONTACT_US}> Contact Us </a>
                     </section>
-                    {/* site logo */}
-                    <Brand location="footer"/>
-                </section>
+                    <section className="Footer-Bottom">
+                        {/* social media icons + links */}
+                        <section className="Footer-Social-Media">
+                            {/* facebook */}
+                            <a href="https://www.facebook.com/">
+                                <AiOutlineFacebook
+                                    style={{ color: "white", borderRadius: "100px" }}
+                                    onMouseOut={({ target }) => {
+                                        target.style.color = "white";
+                                    }}
+                                    onMouseOver={({ target }) => {
+                                        target.style.color = "#EA8644";
+                                    }}
+                                />
+                            </a>
+                            {/* instagram */}
+                            <a href="https://www.instagram.com/">
+                                <AiOutlineInstagram
+                                    style={{ color: "white" }}
+                                    onMouseOut={({ target }) => {
+                                        target.style.color = "white";
+                                    }}
+                                    onMouseOver={({ target }) => {
+                                        target.style.color = "#EA8644";
+                                    }}
+                                />
+                            </a>
+                        </section>
+                        {/* site logo */}
+                        <Brand location="footer"/>
+                    </section>
+                </div>
             </div>
-        </div>
-    );
+        );
+
+    } else{
+
+        return (
+    
+            // outer relative div is needed so navbar can function properly
+            <div style={{position: "relative"}}>
+                <div className="Footer-Main-Container">
+                    <section className="Footer-Bottom">
+
+                         {/* all linked pages in website  */}
+                        <section className="Footer-Pages">
+                            <a href={SITE_PAGES.HOME}> Home </a>
+                            <a href={SITE_PAGES.CONFERENCES}> Conferences </a>
+                            <a href={SITE_PAGES.RESOURCES_LANDING}> Resouces </a>
+                            <a href={SITE_PAGES.ABOUT_US}> About Us </a>
+                            <a href={SITE_PAGES.CONTACT_US}> Contact Us </a>
+                        </section>
+
+                        {/* social media icons + links */}
+                        <section className="Footer-Social-Media">
+                            {/* facebook */}
+                            <a href="https://www.facebook.com/">
+                                <AiOutlineFacebook
+                                    style={{ color: "white", borderRadius: "100px" }}
+                                    onMouseOut={({ target }) => {
+                                        target.style.color = "white";
+                                    }}
+                                    onMouseOver={({ target }) => {
+                                        target.style.color = "#EA8644";
+                                    }}
+                                />
+                            </a>
+                            {/* instagram */}
+                            <a href="https://www.instagram.com/">
+                                <AiOutlineInstagram
+                                    style={{ color: "white" }}
+                                    onMouseOut={({ target }) => {
+                                        target.style.color = "white";
+                                    }}
+                                    onMouseOver={({ target }) => {
+                                        target.style.color = "#EA8644";
+                                    }}
+                                />
+                            </a>
+                        </section>
+                    </section>
+                     {/* site logo */}
+                     <Brand location="footer"/>
+                </div>
+            </div>
+        );
+
+    }
 }
