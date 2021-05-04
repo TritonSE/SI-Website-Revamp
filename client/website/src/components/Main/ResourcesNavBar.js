8aa005366345fa5caf601b1c369f53a60febf6ac
Aaron <aaron.awesome.kirk@gmail.com>
2021-05-04 00:22:10 -0700

    Navbar clipping issues

--- client/website/src/components/Main/ResourcesNavBar.js
+++ client/website/src/components/Main/ResourcesNavBar.js
@@ -35,7 +35,7 @@ export default function ResourcesNavBar(props) {
     function toggleNav() {
         setNavToggled(!navToggled);
 
-        if (!navToggled) setDivStyle({ zIndex: "1001", position: "relative" });
+        if (!navToggled) setDivStyle({ zIndex: "1001" });
         else
             setTimeout(() => {
                 setDivStyle({ zIndex: "999" });
@@ -53,13 +53,13 @@ export default function ResourcesNavBar(props) {
     }
 
     return (
-        <div style={divStyle}>
+        <>
             {/* Button to toggle menu on mobile */}
             <button id="left-arrow" onClick={toggleNav} onKeyDown={toggleNav} type="button">
                 <img src={LeftArrow} alt="Toggle Resources Navigation" />
             </button>
 
-            <div className={`resources-nav ${navToggled ? "toggled" : ""}`}>
+            <div className={`resources-nav ${navToggled ? "toggled" : ""}`} style={divStyle}>
                 {/* Button to close menu on mobile */}
                 <button id="cross" onClick={toggleNav} onKeyDown={toggleNav} type="button">
                     <img src={Cross} alt="Close Resources Navigation" />
@@ -82,6 +82,6 @@ export default function ResourcesNavBar(props) {
 
             {/* Allows for remanining page content to be rendered */}
             {props.children}
-        </div>
+        </>
     );
 }
