7aa5d67889150cc9e33761dca65907f8af5dab63
Amrit-S <aksingh@ucsd.edu>
2021-05-03 18:08:57 -0700

    prettier files

--- client/website/src/components/Main/Footer.js
+++ client/website/src/components/Main/Footer.js
@@ -1,11 +1,10 @@
 import React from "react";
-import {SITE_PAGES} from "../../constants/links";
+import { AiOutlineFacebook, AiOutlineInstagram } from "react-icons/ai";
+import { SITE_PAGES } from "../../constants/links";
 import "../../css/Footer.css";
 
-import {AiOutlineFacebook, AiOutlineInstagram} from "react-icons/ai";
-
 export default function Footer() {
-    return(
+    return (
         <div className="Footer-Main-Container">
             <section className="Footer-Pages">
                 <a href={SITE_PAGES.HOME}> Home </a>
@@ -17,22 +16,22 @@ export default function Footer() {
             <section className="Footer-Bottom">
                 <section className="Footer-Social-Media">
                     <a href="https://material-ui.com/components/icons/">
-                        <AiOutlineFacebook 
-                        style={{color: "white"}}
-                        onMouseOut={({target})=>target.style.color='white'}
-                        onMouseOver={({target})=>target.style.color='#EA8644'} 
+                        <AiOutlineFacebook
+                            style={{ color: "white" }}
+                            // onMouseOut={({ target }) => (target.style.color = "white")}
+                            // onMouseOver={({ target }) => (target.style.color = "#EA8644")}
                         />
                     </a>
                     <a href="https://material-ui.com/components/icons/">
-                        <AiOutlineInstagram 
-                        style={{color: "white"}}
-                        onMouseOut={({target})=>target.style.color='white'}
-                        onMouseOver={({target})=>target.style.color='#EA8644'} 
+                        <AiOutlineInstagram
+                            style={{ color: "white" }}
+                            // onMouseOut={({ target }) => (target.style.color = "white")}
+                            // onMouseOver={({ target }) => (target.style.color = "#EA8644")}
                         />
                     </a>
                 </section>
                 <p> logo </p>
             </section>
-         </div>
+        </div>
     );
 }
--- client/website/src/components/Main/PageLayout.js
+++ client/website/src/components/Main/PageLayout.js
@@ -4,14 +4,14 @@ import NavBar from "./NavBar";
 import Footer from "./Footer";
 
 export default function PageLayout(props) {
-    return(
+    return (
         <body>
             {/* minheight ensures footer is always at the bottom of the page */}
-            <div style={{ minHeight: "calc(100vh - 375px)"}}>
-                <NavBar/>
+            <div style={{ minHeight: "calc(100vh - 375px)" }}>
+                <NavBar />
                 {props.children}
             </div>
-            <Footer/>
+            <Footer />
         </body>
     );
 }
