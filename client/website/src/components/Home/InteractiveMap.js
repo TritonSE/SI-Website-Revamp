2511289c1baac18d765739d887a9f8f63d630a52
Amrit-S <aksingh@ucsd.edu>
2021-05-19 02:53:10 -0700

    finished maps section

--- client/website/src/components/Home/InteractiveMap.js
+++ client/website/src/components/Home/InteractiveMap.js
@@ -1,22 +1,42 @@
-import React, {useState} from "react";
-import { ComposableMap, Geographies, Geography, Marker, ZoomableGroup } from "react-simple-maps";
+import React from "react";
+import {
+    ComposableMap,
+    Geographies,
+    Geography,
+    Marker,
+    ZoomableGroup,
+    Graticule,
+} from "react-simple-maps";
 import "../../css/InteractiveMap.css";
 
 const geoUrl =
     "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
 
-export default function InteractiveMap({setTooltipContent}) {
-
+export default function InteractiveMap({ setTooltipContent }) {
     const markers = [
         {
             name: "Buenos Aires",
             coordinates: [-58.3816, -34.6037],
             isBranch: true,
             email: "kingnavid@awesome.com",
-            urlLink: "https://www.instagram.com/navidisme/?hl=en"
+            urlLink: "https://www.instagram.com/navidisme/?hl=en",
         },
-        {  name: "La Paz", coordinates: [-68.1193, -16.4897], isBranch: true, email: "thomas@cat.com", urlLink: "https://tse.ucsd.edu/static/3122a8820897b8d1110b0840edc0944e/a41d1/Thomas_Garry.jpg" },
-        {  name: "Brasilia", coordinates: [-47.8825, -15.7942], isBranch: false, email: "patrick@weirdo.com", urlLink: "https://tse.ucsd.edu/static/50db41fd31f90dfd8ae4958102b7476a/ed396/Patrick_Brown.png" },
+        {
+            name: "La Paz",
+            coordinates: [-68.1193, -16.4897],
+            isBranch: true,
+            email: "thomas@cat.com",
+            urlLink:
+                "https://tse.ucsd.edu/static/3122a8820897b8d1110b0840edc0944e/a41d1/Thomas_Garry.jpg",
+        },
+        {
+            name: "Brasilia",
+            coordinates: [-47.8825, -15.7942],
+            isBranch: false,
+            email: "patrick@weirdo.com",
+            urlLink:
+                "https://tse.ucsd.edu/static/50db41fd31f90dfd8ae4958102b7476a/ed396/Patrick_Brown.png",
+        },
         // { markerOffset: 15, name: "Santiago", coordinates: [-70.6693, -33.4489] },
         // { markerOffset: 15, name: "Bogota", coordinates: [-74.0721, 4.711] },
         // { markerOffset: 15, name: "Quito", coordinates: [-78.4678, -0.1807] },
@@ -29,49 +49,48 @@ export default function InteractiveMap({setTooltipContent}) {
     ];
 
     return (
-
         <div className="Interactive-Map">
-            <p> Test </p>
             <ComposableMap>
                 <ZoomableGroup zoom={1}>
+                    <Graticule stroke="#EAEAEC" />
                     <Geographies geography={geoUrl}>
                         {({ geographies }) =>
                             geographies.map((geo) => <Geography key={geo.rsmKey} geography={geo} />)
                         }
                     </Geographies>
-                    {markers.map(({ name, coordinates, isBranch, email, urlLink}) => (
+                    {markers.map(({ name, coordinates, isBranch, email, urlLink }) => (
                         <a href={urlLink} target="_blank" rel="noreferrer noopener">
-                               <Marker 
-                        data-tip="hover me"
-                        key={name} 
-                        coordinates={coordinates} 
-                        onMouseEnter={() => {
-                            setTooltipContent({
-                                country: name,
-                                email: email,
-                                urlLink: "www.google.com"
-                            });
-                          }}
-                          onMouseLeave={() => {
-                            setTooltipContent({
-                                country: "",
-                                email: "",
-                                urlLink: ""
-                            });
-                          }}
-                        >
-                            <g
-                                fill="none"
-                                stroke={isBranch ? "#d77a3d":"#6652a0"}
-                                strokeWidth="2"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                transform="translate(-12, -24)"
+                            <Marker
+                                data-tip="hover me"
+                                key={name}
+                                coordinates={coordinates}
+                                onMouseEnter={() => {
+                                    setTooltipContent({
+                                        country: name,
+                                        email: email,
+                                        urlLink: urlLink,
+                                    });
+                                }}
+                                onMouseLeave={() => {
+                                    setTooltipContent({
+                                        country: "",
+                                        email: "",
+                                        urlLink: "",
+                                    });
+                                }}
                             >
-                                <circle cx="12" cy="10" r="4" fill="white"/>
-                                <path d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z" />
-                            </g>
-                        </Marker>
+                                <g
+                                    fill="none"
+                                    stroke={isBranch ? "#d77a3d" : "#6652a0"}
+                                    strokeWidth="2"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    transform="translate(-12, -24)"
+                                >
+                                    <circle cx="12" cy="10" r="4" fill="white" />
+                                    <path d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z" />
+                                </g>
+                            </Marker>
                         </a>
                     ))}
                 </ZoomableGroup>
--- client/website/src/pages/Home.js
+++ client/website/src/pages/Home.js
@@ -1,14 +1,14 @@
-import React, {useState} from "react";
-import InteractiveMap from "../components/Home/InteractiveMap";
-import ReactTooltip from "react-tooltip";
-import "../css/Home.css";
+import React, { useState } from "react";
 import { FiExternalLink } from "react-icons/fi";
+import ReactTooltip from "react-tooltip";
+import InteractiveMap from "../components/Home/InteractiveMap";
+import "../css/Home.css";
 
 export default function Home() {
     const [content, setTooltipContent] = useState({
         country: "",
         email: "",
-        urlLink: ""
+        urlLink: "",
     });
 
     return (
@@ -38,18 +38,41 @@ export default function Home() {
                 </section>
                 <hr className="divider" />
                 <section id="branches-and-chapters">
-                    {/* <p  data-tip="hover me"> Hello </p>
-                    <p  data-tip="check me"> Hello </p> */}
-                    <a data-tip='custom show' data-event='click focus'>( •̀д•́)</a>
                     <InteractiveMap setTooltipContent={setTooltipContent} />
-                    <ReactTooltip place="left" effect="solid" type="dark" delayUpdate={1} globalEventOff='click' >
-                        <a href={content.urlLink} target="_blank">
+                    <ReactTooltip
+                        place="left"
+                        effect="solid"
+                        type="dark"
+                        delayUpdate={1}
+                        globalEventOff="click"
+                    >
+                        <a href={content.urlLink} target="_blank" rel="noreferrer" >
                             {content.country}
-                            <FiExternalLink/>
+                            <FiExternalLink />
                         </a>
-                        <br/>
+                        <br />
                         {content.email}
                     </ReactTooltip>
+                    <div className="branch-info">
+                        <h1>Branches </h1>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+                            facilisis condimentum massa, sit amet lacinia massa commodo sed.
+                            {"\n\n\n"}
+                            Hover over each pin for more information about each branch!
+                        </p>
+                        <section className="legend-container">
+                            <div className="label">
+                                <div className="color-box" style={{ backgroundColor: "#EA8644" }} />
+                                <div className="tag"> Branch </div>
+                            </div>
+
+                            <div className="label">
+                                <div className="color-box" style={{ backgroundColor: "#8477B9" }} />
+                                <div className="tag"> Chapter </div>
+                            </div>
+                        </section>
+                    </div>
                 </section>
                 <section id="home-be-involved">
                     <h1>Be Involved </h1>
