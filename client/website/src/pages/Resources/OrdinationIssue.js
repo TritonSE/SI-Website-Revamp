dc6c9354b1437a8b275a5de8c31fc7565f462fa4
Dhanush <dhanunanjunda2@gmail.com>
2021-05-21 16:51:31 -0700

    finished desktop ordination page

--- client/website/src/pages/Resources/OrdinationIssue.js
+++ client/website/src/pages/Resources/OrdinationIssue.js
@@ -1,9 +1,24 @@
 import React from "react";
+import "../../css/OrdinationIssue.css";
+
+import HeaderImage from "../../media/JoinUs_Header.png";
+
+import OrdinationImage from "../../media/OrdinationIssue_Image.png";
 
 export default function OrdinationIssue() {
     return (
         <div>
-            <p> This is the Ordination Issue Page </p>
+            <div className="header-div">
+                <img src={HeaderImage} alt="Header Image" id="Header" />
+                <div className="header-info-div">
+                    <h1 className="header-text">The Ordination Issue</h1>
+                </div>
+            </div>
+            <div className="page-content">
+                <p className="ordination-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas facilisis condimentum massa, sit amet lacinia massa commodo sed. Praesent vehicula eget arcu ut laoreet. Sed porta, dui ut dapibus sodales, orci neque volutpat arcu, in efficitur sem tortor vel lectus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas facilisis condimentum massa, sit amet lacinia massa commodo sed. Praesent vehicula eget arcu ut laoreet. Sed porta, dui ut dapibus sodales, orci neque volutpat arcu, in efficitur sem tortor vel lectus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas facilisis condimentum massa, sit amet lacinia massa commodo sed. Praesent vehicula eget arcu ut laoreet. Sed porta, dui ut dapibus sodales, orci neque volutpat arcu, in efficitur sem tortor vel lectus.Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <img src={OrdinationImage} alt="Ordination Image" className="ordination-image"/>
+                <p className="ordination-text bottom-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas facilisis condimentum massa, sit amet lacinia massa commodo sed. Praesent vehicula eget arcu ut laoreet. Sed porta, dui ut dapibus sodales, orci neque volutpat arcu, in efficitur sem tortor vel lectus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas facilisis condimentum massa, sit amet lacinia massa commodo sed. Praesent vehicula eget arcu ut laoreet. Sed porta, dui ut dapibus sodales, orci neque volutpat arcu, in efficitur sem tortor vel lectus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas facilisis condimentum massa, sit amet lacinia massa commodo sed. Praesent vehicula eget arcu ut laoreet. Sed porta, dui ut dapibus sodales, orci neque volutpat arcu, in efficitur sem tortor vel lectus.Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
         </div>
     );
 }
