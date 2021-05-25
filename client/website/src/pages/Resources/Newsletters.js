3f47c4a4d22f1dc7bf8f0647b6fe7fe955e3fdb8
PatrickBrown1 <patrickbrown1312@gmail.com>
2021-05-25 14:15:34 -0700

    updated mobile styles

--- client/website/src/pages/Resources/Newsletters.js
+++ client/website/src/pages/Resources/Newsletters.js
@@ -30,7 +30,7 @@ const calculateCards = (newsletters, currentPage, numPerPage) =>
     newsletters.slice(currentPage * numPerPage, (currentPage + 1) * numPerPage);
 
 // renders the current newsletters from props in a grid
-const renderPublicationGrid = (currentNewsletters) => (
+const renderPublicationGrid = (currentNewsletters, isMobile) => (
     <div className="NewsletterContainer">
         {currentNewsletters.map((newsletter) => (
             <NewsletterCard
@@ -39,6 +39,7 @@ const renderPublicationGrid = (currentNewsletters) => (
                 year={newsletter.year}
                 image_url={newsletter.image_url}
                 redirect_link={newsletter.redirect_url}
+                isMobile={isMobile}
             />
         ))}
     </div>
@@ -48,7 +49,7 @@ export default function Newsletters() {
     const [maxPages, setMaxPages] = useState(9);
     const [numPerPage, setNumPerPage] = useState(9);
     const [currentPage, setCurrentPage] = useState(0);
-
+    const [isMobile, setIsMobile] = useState(false);
     // track window resizes to determine rerender
     useEffect(() => {
         function handleResize() {
@@ -65,6 +66,12 @@ export default function Newsletters() {
                 setNumPerPage(9);
                 setMaxPages(Math.ceil(newsletterList.length / 9));
             }
+
+            if (window.innerWidth <= 400) {
+                setIsMobile(true);
+            } else {
+                setIsMobile(false);
+            }
         }
         // Add event listener
         window.addEventListener("resize", handleResize);
@@ -76,23 +83,43 @@ export default function Newsletters() {
 
     return (
         <>
-            <ResourcesHeader
-                title="Sakyadhita Newsletter"
-                text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas facilisis condimentum massa, sit amet lacinia massa commodo sed. Praesent vehicula eget arcu ut laoreet. Sed porta, dui ut dapibus sodales, orci neque volutpat arcu, in efficitur sem tortor vel lectus. "
-                image="https://s3-alpha-sig.figma.com/img/2632/7b67/21741a7000235fce33b207b71e8da1cd?Expires=1622419200&Signature=NaKrOXhTogZlNaWaE-kcQHGyv7cZL745P19PBIu3a~8UWgMEh0wqj3bAmfbhisl4j91lMJQei5kgDCFq4KIUeqRX39BNJxg7qt8CKrpx1Hw-kdUaaTz-10Rqp6tw~vxCma9x19Ry~JIhDTRyAjyn2zN21NTYCNvF1Xpc2BgzzavRdZsW22cFD66fde8NG-Sqq85fT4QwvDxStiavUn0EqvAYPvYDxJ2uhghbnI6U07n5MmEI~nrUzV2IamBqywS-B5TMo7Qqxnajzf1H6WgNKrFRVFok5jNVy4J4HNXYJLp7lQFXZ1yzs9YD~bapJXBg1Yo9R6KTWsv58zxvtw2M6g__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
-                height="100%"
-                width="100%"
-            />
+            {isMobile ? (
+                <ResourcesHeader
+                    title="Sakyadhita Newsletters"
+                    text=""
+                    image="https://s3-alpha-sig.figma.com/img/2632/7b67/21741a7000235fce33b207b71e8da1cd?Expires=1622419200&Signature=NaKrOXhTogZlNaWaE-kcQHGyv7cZL745P19PBIu3a~8UWgMEh0wqj3bAmfbhisl4j91lMJQei5kgDCFq4KIUeqRX39BNJxg7qt8CKrpx1Hw-kdUaaTz-10Rqp6tw~vxCma9x19Ry~JIhDTRyAjyn2zN21NTYCNvF1Xpc2BgzzavRdZsW22cFD66fde8NG-Sqq85fT4QwvDxStiavUn0EqvAYPvYDxJ2uhghbnI6U07n5MmEI~nrUzV2IamBqywS-B5TMo7Qqxnajzf1H6WgNKrFRVFok5jNVy4J4HNXYJLp7lQFXZ1yzs9YD~bapJXBg1Yo9R6KTWsv58zxvtw2M6g__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
+                    height="40%"
+                    width="100%"
+                />
+            ) : (
+                <ResourcesHeader
+                    title="Sakyadhita Newsletters"
+                    text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas facilisis condimentum massa, sit amet lacinia massa commodo sed. Praesent vehicula eget arcu ut laoreet. Sed porta, dui ut dapibus sodales, orci neque volutpat arcu, in efficitur sem tortor vel lectus. "
+                    image="https://s3-alpha-sig.figma.com/img/2632/7b67/21741a7000235fce33b207b71e8da1cd?Expires=1622419200&Signature=NaKrOXhTogZlNaWaE-kcQHGyv7cZL745P19PBIu3a~8UWgMEh0wqj3bAmfbhisl4j91lMJQei5kgDCFq4KIUeqRX39BNJxg7qt8CKrpx1Hw-kdUaaTz-10Rqp6tw~vxCma9x19Ry~JIhDTRyAjyn2zN21NTYCNvF1Xpc2BgzzavRdZsW22cFD66fde8NG-Sqq85fT4QwvDxStiavUn0EqvAYPvYDxJ2uhghbnI6U07n5MmEI~nrUzV2IamBqywS-B5TMo7Qqxnajzf1H6WgNKrFRVFok5jNVy4J4HNXYJLp7lQFXZ1yzs9YD~bapJXBg1Yo9R6KTWsv58zxvtw2M6g__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
+                    height="100%"
+                    width="100%"
+                />
+            )}
+
             <div className="NewsletterPage">
-                <h1 className="NewsletterPage__title">Latest</h1>
-                {renderPublicationGrid(calculateCards(newsletterList, currentPage, numPerPage))}
+                <h1
+                    className={
+                        !isMobile ? "NewsletterPage__title" : "NewsletterPage__title--mobile"
+                    }
+                >
+                    Latest
+                </h1>
+                {renderPublicationGrid(
+                    calculateCards(newsletterList, currentPage, numPerPage),
+                    isMobile
+                )}
                 <ReactPaginate
                     previousLabel="<"
                     nextLabel=">"
                     breakLabel="..."
                     pageCount={maxPages}
-                    marginPagesDisplayed={2}
-                    pageRangeDisplayed={3}
+                    marginPagesDisplayed={1}
+                    pageRangeDisplayed={2}
                     onPageChange={(e) => setCurrentPage(e.selected)}
                     containerClassName="newsletter__pagination"
                     activeClassName="newsletter__pagination--active"
