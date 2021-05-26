232397fd984cea7776e1aec35df49d00e8e29972
PatrickBrown1 <patrickbrown1312@gmail.com>
2021-05-25 20:06:21 -0700

    added orange arrows and log out button

--- client/admin-tool/src/components/NavBar.js
+++ client/admin-tool/src/components/NavBar.js
@@ -1,64 +1,153 @@
-import React from "react";
-import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+import React, { useState, useEffect, useRef } from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
 import { SITE_PAGES } from "../constants/links";
 import "../css/NavBar.css";
 
 export default function NavBar() {
+    const [expanded, setExpanded] = useState("");
+    const navRef = useRef(null);
+
+    useEffect(() => {
+        // reset expanded if click outside of nav
+        function handleClickOutsideNav(event) {
+            if (navRef.current && !navRef.current.contains(event.target)) {
+                setExpanded("");
+            }
+        }
+
+        // Bind the event listener
+        document.addEventListener("mousedown", handleClickOutsideNav);
+        return () => {
+            // Unbind the event listener on clean up
+            document.removeEventListener("mousedown", handleClickOutsideNav);
+        };
+    }, [navRef]);
+
     return (
-        <Navbar expand="lg" className="nav-bar-container">
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ml-auto">
-                    <NavDropdown title="Home" id="basic-nav-dropdown">
-                        <NavDropdown.Item href={SITE_PAGES.HOME_NEWS_AND_EVENTS_SLIDER}>
-                            News & Events Slider
-                        </NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href={SITE_PAGES.HOME_INTRODUCTION}>
-                            Introduction
-                        </NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href={SITE_PAGES.HOME_BRANCHES_CHAPTERS}>
-                            Branches & Chapters
-                        </NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href={SITE_PAGES.HOME_ADD_SECTION}>
-                            + Section
-                        </NavDropdown.Item>
-                        <NavDropdown.Divider />
-                    </NavDropdown>
-                    <Nav.Link href={SITE_PAGES.CONFERENCES} className="nav-item">
-                        Conferences
-                    </Nav.Link>
-                    <NavDropdown title="Resources" id="basic-nav-dropdown">
-                        <NavDropdown.Item href={SITE_PAGES.RESOURCE_NEWSLETTERS}>
-                            Newsletters
-                        </NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href={SITE_PAGES.RESOURCE_EPUBS}>
-                            E-Publications
-                        </NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href={SITE_PAGES.RESOURCE_BUDDHIST_CULTURE}>
-                            Buddhist Culture
-                        </NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href={SITE_PAGES.RESOURCE_ORDINATION_ISSUE}>
-                            Ordination Issue
-                        </NavDropdown.Item>
-                        <NavDropdown.Divider />
-                    </NavDropdown>
-                    <NavDropdown title="About Us" id="basic-nav-dropdown">
-                        <NavDropdown.Item href={SITE_PAGES.ABOUT_EDIT_SECTION}>
-                            About Us - All
-                        </NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href={SITE_PAGES.ABOUT_EXEC_COMMITTEE}>
-                            Executive Committee
-                        </NavDropdown.Item>
-                    </NavDropdown>
-                </Nav>
-            </Navbar.Collapse>
+        <Navbar expand="lg" className="nav-bar-container" ref={navRef}>
+            <Container>
+                {/* Home Button */}
+                <Navbar.Toggle
+                    collapseOnSelect
+                    aria-controls="responsiv-navbar-nav"
+                    onClick={() => {
+                        setExpanded(expanded === "home" ? "" : "home");
+                    }}
+                >
+                    Home
+                    {expanded === "home" && (
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="28"
+                            viewBox="0 0 24 28"
+                            fill="none"
+                            className="navOrangeArrow"
+                        >
+                            <path
+                                d="M1.90798e-07 14L24 0.143593L24 27.8564L1.90798e-07 14Z"
+                                fill="#EA8644"
+                            />
+                        </svg>
+                    )}
+                </Navbar.Toggle>
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    {expanded === "home" && (
+                        <Nav>
+                            <Nav.Link href={SITE_PAGES.HOME_NEWS_AND_EVENTS_SLIDER}>
+                                News & Events Slider
+                            </Nav.Link>
+                            <Nav.Link href={SITE_PAGES.HOME_INTRODUCTION}>Introduction</Nav.Link>
+                            <Nav.Link href={SITE_PAGES.HOME_BRANCHES_CHAPTERS}>
+                                Branches & Chapters
+                            </Nav.Link>
+                            <Nav.Link href={SITE_PAGES.HOME_ADD_SECTION}>+ Section</Nav.Link>
+                        </Nav>
+                    )}
+                </Navbar.Collapse>
+
+                {/* Conferences Button */}
+                <Nav.Link href={SITE_PAGES.CONFERENCES}>Conferences</Nav.Link>
+
+                {/* Resources Button */}
+                <Navbar.Toggle
+                    collapseOnSelect
+                    aria-controls="responsiv-navbar-nav"
+                    onClick={() => {
+                        setExpanded(expanded === "resources" ? "" : "resources");
+                    }}
+                >
+                    Resources
+                    {expanded === "resources" && (
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="28"
+                            viewBox="0 0 24 28"
+                            fill="none"
+                            className="navOrangeArrow"
+                        >
+                            <path
+                                d="M1.90798e-07 14L24 0.143593L24 27.8564L1.90798e-07 14Z"
+                                fill="#EA8644"
+                            />
+                        </svg>
+                    )}
+                </Navbar.Toggle>
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    {expanded === "resources" && (
+                        <Nav>
+                            <Nav.Link href={SITE_PAGES.RESOURCE_NEWSLETTERS}>Newsletters</Nav.Link>
+                            <Nav.Link href={SITE_PAGES.RESOURCE_EPUBS}>E-Publications</Nav.Link>
+                            <Nav.Link href={SITE_PAGES.RESOURCE_BUDDHIST_CULTURE}>
+                                Buddhist Culture
+                            </Nav.Link>
+                            <Nav.Link href={SITE_PAGES.RESOURCE_ORDINATION_ISSUE}>
+                                Ordination Issue
+                            </Nav.Link>
+                        </Nav>
+                    )}
+                </Navbar.Collapse>
+
+                {/* About Us Button */}
+                <Navbar.Toggle
+                    collapseOnSelect
+                    aria-controls="responsiv-navbar-nav"
+                    onClick={() => {
+                        setExpanded(expanded === "aboutus" ? "" : "aboutus");
+                    }}
+                >
+                    About Us
+                    {expanded === "aboutus" && (
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="28"
+                            viewBox="0 0 24 28"
+                            fill="none"
+                            className="navOrangeArrow"
+                        >
+                            <path
+                                d="M1.90798e-07 14L24 0.143593L24 27.8564L1.90798e-07 14Z"
+                                fill="#EA8644"
+                            />
+                        </svg>
+                    )}
+                </Navbar.Toggle>
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    {expanded === "aboutus" && (
+                        <Nav>
+                            <Nav.Link href={SITE_PAGES.ABOUT_EDIT_SECTION}>About Us - All</Nav.Link>
+                            <Nav.Link href={SITE_PAGES.ABOUT_EXEC_COMMITTEE}>
+                                Executive Committee
+                            </Nav.Link>
+                        </Nav>
+                    )}
+                </Navbar.Collapse>
+
+                {/* Log Out Button */}
+                <Nav.Link className="logout_button">Log Out</Nav.Link>
+            </Container>
         </Navbar>
     );
 }
