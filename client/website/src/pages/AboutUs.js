2b9a98c0bdd481ed58614452d8d48a7c539b6896
PatrickBrown1 <patrickbrown1312@gmail.com>
2021-05-30 18:09:30 -0700

    moved test component to 404 page from about us

--- client/website/src/pages/AboutUs.js
+++ client/website/src/pages/AboutUs.js
@@ -1,22 +1,9 @@
 import React from "react";
-import PayPal from "../components/PayPal";
 
 export default function AboutUs() {
     return (
         <div>
             <p> This is the About Us Page </p>
-            <PayPal
-                membershipTitle="Nun/Student/Unemployed"
-                membershipID={1}
-                membershipCost={15}
-                donationAmount={0}
-                isNewMember
-                affiliatedOrgs="affiliated org 1"
-                disable={false}
-                transactionCompleted={() => {
-                    alert("completed transaction yay");
-                }}
-            />
         </div>
     );
 }
