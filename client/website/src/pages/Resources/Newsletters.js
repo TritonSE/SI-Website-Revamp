989966c0e8b80dc9489d9435ee849b4bb49b261f
PatrickBrown1 <patrickbrown1312@gmail.com>
2021-06-03 16:51:59 -0700

    updated picture for newsletters page

--- client/website/src/pages/Resources/Newsletters.js
+++ client/website/src/pages/Resources/Newsletters.js
@@ -11,6 +11,8 @@ import ResourcesHeader from "../../components/ResourcesHeader";
 import NewsletterCard from "../../components/Newsletters/NewsletterCard";
 import "../../css/Newsletters.css";
 
+import Header from "../../media/Lotus_Header.png";
+
 // fill newsletterList
 const newsletterList = [];
 let i = 0;
@@ -94,7 +96,7 @@ export default function Newsletters() {
                 <ResourcesHeader
                     title="Sakyadhita Newsletters"
                     text=""
-                    image="https://s3-alpha-sig.figma.com/img/2632/7b67/21741a7000235fce33b207b71e8da1cd?Expires=1622419200&Signature=NaKrOXhTogZlNaWaE-kcQHGyv7cZL745P19PBIu3a~8UWgMEh0wqj3bAmfbhisl4j91lMJQei5kgDCFq4KIUeqRX39BNJxg7qt8CKrpx1Hw-kdUaaTz-10Rqp6tw~vxCma9x19Ry~JIhDTRyAjyn2zN21NTYCNvF1Xpc2BgzzavRdZsW22cFD66fde8NG-Sqq85fT4QwvDxStiavUn0EqvAYPvYDxJ2uhghbnI6U07n5MmEI~nrUzV2IamBqywS-B5TMo7Qqxnajzf1H6WgNKrFRVFok5jNVy4J4HNXYJLp7lQFXZ1yzs9YD~bapJXBg1Yo9R6KTWsv58zxvtw2M6g__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
+                    image={Header}
                     height="max(40%, 300px)"
                     width="100%"
                 />
@@ -102,7 +104,7 @@ export default function Newsletters() {
                 <ResourcesHeader
                     title="Sakyadhita Newsletters"
                     text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas facilisis condimentum massa, sit amet lacinia massa commodo sed. Praesent vehicula eget arcu ut laoreet. Sed porta, dui ut dapibus sodales, orci neque volutpat arcu, in efficitur sem tortor vel lectus. "
-                    image="https://s3-alpha-sig.figma.com/img/2632/7b67/21741a7000235fce33b207b71e8da1cd?Expires=1622419200&Signature=NaKrOXhTogZlNaWaE-kcQHGyv7cZL745P19PBIu3a~8UWgMEh0wqj3bAmfbhisl4j91lMJQei5kgDCFq4KIUeqRX39BNJxg7qt8CKrpx1Hw-kdUaaTz-10Rqp6tw~vxCma9x19Ry~JIhDTRyAjyn2zN21NTYCNvF1Xpc2BgzzavRdZsW22cFD66fde8NG-Sqq85fT4QwvDxStiavUn0EqvAYPvYDxJ2uhghbnI6U07n5MmEI~nrUzV2IamBqywS-B5TMo7Qqxnajzf1H6WgNKrFRVFok5jNVy4J4HNXYJLp7lQFXZ1yzs9YD~bapJXBg1Yo9R6KTWsv58zxvtw2M6g__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
+                    image={Header}
                     height="100%"
                     width="100%"
                 />
