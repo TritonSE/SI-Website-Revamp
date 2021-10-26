12a69c40efe7a6a82fe8b84d3da9c7dd2cc3e80d
Jacob Au <jacobau.inbox@gmail.com>
2021-10-25 19:57:10 -0700

    fixed dropdown

--- client/admin-tool/src/pages/Home/BranchesChapters.js
+++ client/admin-tool/src/pages/Home/BranchesChapters.js
@@ -1,7 +1,6 @@
 import React from "react";
-import { TextField, Snackbar, Select, MenuItem } from "@material-ui/core";
-
-import { makeStyles } from "@material-ui/core/styles";
+import { TextField, Snackbar, MenuItem } from "@material-ui/core";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
 import Button from "../../components/Button";
 import "../../css/BranchesChapters.css";
 
@@ -15,15 +14,6 @@ const useStyles = makeStyles((theme) => ({
         "& .MuiTextField-root": {
             margin: theme.spacing(1),
             width: "95%",
-        },
-        "& .MuiSelect-root .MuiMenuItem-root": {
-            //margin: theme.spacing(1),
-            minWidth: "calc(95%)",
-            width: '95%',
-        },
-        Select: {
-            minWidth: "calc(95%)",
-
         },
 
         // default rendering of field
@@ -44,6 +34,49 @@ const useStyles = makeStyles((theme) => ({
         },
     },
 }));
+
+const CustomSelectField = withStyles({
+    root: {
+        "& .MuiOutlinedInput-input": {
+            color: "#000000",
+        },
+        "& .MuiInputLabel-root": {
+            // color: "black",
+            // fontFamily: "Nunito",
+            // fontSize: "18px",
+            lineHeight: "25px",
+            opacity: "1",
+        },
+        "& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
+            borderColor: "black",
+            border: "1px solid #000000",
+            borderRadius: "30px",
+            fontFamily: "Nunito",
+            fontSize: "18px",
+            lineHeight: "1.95vw",
+            boxSizing: "border-box",
+            color: "#000000",
+        },
+        "&:hover .MuiOutlinedInput-input": {
+            color: "black",
+        },
+        "&:hover .MuiInputLabel-root": {
+            color: "black",
+        },
+        "&:hover .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
+            borderColor: "black",
+        },
+        "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-input": {
+            color: "black",
+        },
+        "& .MuiInputLabel-root.Mui-focused": {
+            color: "black",
+        },
+        "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
+            borderColor: "#6652a0",
+        },
+    },
+})(TextField);
 
 export default function BranchesChapters() {
     const classes = useStyles();
@@ -137,11 +170,9 @@ export default function BranchesChapters() {
             return;
         }
 
-        if(values.isBranch.value === "branch"){
+        if (values.isBranch.value === "branch") {
             isBranch = true;
-            console.log("hi");
-        }
-        else{
+        } else {
             isBranch = false;
         }
 
@@ -154,7 +185,7 @@ export default function BranchesChapters() {
                 email: values.email.value,
                 longitude: values.longitude.value,
                 latitude: values.latitude.value,
-                isBranch: isBranch,
+                isBranch,
                 siteLink: values.siteLink.value,
             }),
         }).then((res) => {
@@ -191,85 +222,91 @@ export default function BranchesChapters() {
                 <h2>Branches & Chapters</h2>
                 <form className={classes.form} autoComplete="off">
                     {/* Full Name of Branch Chapter Field */}
-                    <div className="form-field-wrapper">
-                        <TextField
-                            name="name"
-                            value={values.name.value}
-                            error={values.name.error}
-                            onChange={handleChange}
-                            placeholder="Name of Branch"
-                            disabled={isFormDisabled}
-                            variant="outlined"
-                        />
-                        <span className="required-asterisk"> * </span>
-                    </div>
-                    {/* Longitute of Branch/Chapter */}
-                    <div className="form-field-wrapper">
-                        <TextField
-                            name="latitude"
-                            value={values.latitude.value}
-                            error={values.latitude.error}
-                            onChange={handleChange}
-                            placeholder="Latitude"
-                            disabled={isFormDisabled}
-                            variant="outlined"
-                        />
-                        <span className="required-asterisk"> * </span>
-                    </div>
-                    {/* Latitude of Branch/Chapter */}
-                    <div className="form-field-wrapper">
-                        <TextField
-                            name="longitude"
-                            value={values.longitude.value}
-                            error={values.longitude.error}
-                            onChange={handleChange}
-                            placeholder="Longitude"
-                            disabled={isFormDisabled}
-                            variant="outlined"
-                        />
-                        <span className="required-asterisk"> * </span>
-                    </div>
-                    {/* Type of Branch/Chapter */}
-                    <div className="form-field-wrapper">
-                        <Select
-                            name="isBranch"
-                            error={values.isBranch.error}
-                            value={values.isBranch.value}
-                            placeholder="Branch"
-                            onChange={handleChange}
-                            disabled={isFormDisabled}
-                            variant="outlined"
-                        >
-                            <MenuItem value="branch">Branch</MenuItem>
-                            <MenuItem value="chapter">Chapter</MenuItem>
-                        </Select>
+                    <div>
+                        <div className="form-field-wrapper">
+                            <TextField
+                                name="name"
+                                value={values.name.value}
+                                error={values.name.error}
+                                onChange={handleChange}
+                                placeholder="Name of Branch"
+                                disabled={isFormDisabled}
+                                variant="outlined"
+                            />
+                            <span className="required-asterisk"> * </span>
+                        </div>
+                        {/* Longitute of Branch/Chapter */}
+                        <div className="form-field-wrapper">
+                            <TextField
+                                name="latitude"
+                                value={values.latitude.value}
+                                error={values.latitude.error}
+                                onChange={handleChange}
+                                placeholder="Latitude"
+                                disabled={isFormDisabled}
+                                variant="outlined"
+                            />
+                            <span className="required-asterisk"> * </span>
+                        </div>
+                        {/* Latitude of Branch/Chapter */}
+                        <div className="form-field-wrapper">
+                            <TextField
+                                name="longitude"
+                                value={values.longitude.value}
+                                error={values.longitude.error}
+                                onChange={handleChange}
+                                placeholder="Longitude"
+                                disabled={isFormDisabled}
+                                variant="outlined"
+                            />
+                            <span className="required-asterisk"> * </span>
+                        </div>
+                        {/* Type of Branch/Chapter */}
 
-                        <span className="required-asterisk"> * </span>
-                    </div>
-                    {/* Email of Branch/Chapter */}
-                    <div className="form-field-wrapper">
-                        <TextField
-                            name="email"
-                            value={values.email.value}
-                            error={values.email.error}
-                            onChange={handleChange}
-                            placeholder="Email of Branch"
-                            disabled={isFormDisabled}
-                            variant="outlined"
-                        />
-                        <span className="required-asterisk"> * </span>
-                    </div>
-                    {/* Site Link of Branch/Chapter */}
-                    <div className="form-field-wrapper">
-                        <TextField
-                            name="siteLink"
-                            value={values.siteLink.value}
-                            error={values.siteLink.error}
-                            onChange={handleChange}
-                            placeholder="Site Link"
-                            disabled={isFormDisabled}
-                            variant="outlined"
-                        />
+                        <div className="form-field-wrapper">
+                            <CustomSelectField
+                                name="isBranch"
+                                error={values.isBranch.error}
+                                value={values.isBranch.value}
+                                placeholder="Branch"
+                                onChange={handleChange}
+                                disabled={isFormDisabled}
+                                variant="outlined"
+                                label="Branch"
+                                select
+                                // autoWidth={false}
+                            >
+                                <MenuItem value="branch">Branch</MenuItem>
+                                <MenuItem value="chapter">Chapter</MenuItem>
+                            </CustomSelectField>
+                            <span className="required-asterisk"> * </span>
+                        </div>
+
+                        {/* Email of Branch/Chapter */}
+                        <div className="form-field-wrapper">
+                            <TextField
+                                name="email"
+                                value={values.email.value}
+                                error={values.email.error}
+                                onChange={handleChange}
+                                placeholder="Email of Branch"
+                                disabled={isFormDisabled}
+                                variant="outlined"
+                            />
+                            <span className="required-asterisk"> * </span>
+                        </div>
+                        {/* Site Link of Branch/Chapter */}
+                        <div className="form-field-wrapper">
+                            <TextField
+                                name="siteLink"
+                                value={values.siteLink.value}
+                                error={values.siteLink.error}
+                                onChange={handleChange}
+                                placeholder="Insert a Site Link"
+                                disabled={isFormDisabled}
+                                variant="outlined"
+                            />
+                        </div>
                     </div>
                 </form>
             </section>
