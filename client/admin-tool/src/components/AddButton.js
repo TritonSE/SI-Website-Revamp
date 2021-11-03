96b765c4892ff66de501e8fd49fcbf219b1bc406
Amrit-S <aksingh@ucsd.edu>
2021-11-02 18:06:35 -0700

    comments for add button

--- client/admin-tool/src/components/AddButton.js
+++ client/admin-tool/src/components/AddButton.js
@@ -1,8 +1,8 @@
 /**
- * Reusable button for the admin page. Allows for customization
+ * Reusable button for the admin page, specifically for add functionality. Allows for customization
  * as needed.
  *
- * By default, renders as orange with white text.
+ * By default, renders as purple with white text.
  *
  * @summary Customizable Button Component
  * @author  Amrit Singh
@@ -16,16 +16,18 @@ import "../css/AddButton.css";
 
 /**
  *
+ * @param {function} onClickCallback - Function will be callbacked whenever the button is clicked (REQUIRED)
+ *
  * @param {string} text - Button content as it will be shown to the user
- * @param {function} onClickCallback - Function will be callbacked whenever the button is clicked
+ * @param {JSON} className - Any additional classes to be added to the button (will be given precedent)
  * @param {JSON} style - Any in-line css customizations on the button
  * @returns
  */
 export default function AddButton({
-    text = "Add New",
+    text = "Add New", // default value
     onClickCallback,
-    className = "",
-    style = {},
+    className = "", // default value
+    style = {}, // default value
 }) {
     // The "+" icon on the 'add' button
     const addIcon = <FontAwesomeIcon icon={faPlus} style={{ marginRight: "10px" }} />;
