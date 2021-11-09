393332bc0a0a90ad2c462ab07c19a34dc249361b
Amrit-S <aksingh@ucsd.edu>
2021-11-08 20:28:17 -0800

    started login page, working on form

--- client/admin-tool/src/pages/Accounts/Login.js
+++ client/admin-tool/src/pages/Accounts/Login.js
@@ -1,9 +1,21 @@
 import React from "react";
 
+import TextField from "@mui/material/TextField";
+import { Snackbar } from "@material-ui/core";
+
+import Brand from "../../components/Accounts/Brand";
+import Button from "../../components/Button";
+
+import "../../css/Login.css";
+
 export default function Login() {
     return (
-        <div>
-            <p> This is the Login Page </p>
+        <div id="login-page">
+            <div className="login-form-wrapper">
+                <Brand location="footer"/>
+
+                <Button text="Login" />
+            </div>
         </div>
     );
 }
