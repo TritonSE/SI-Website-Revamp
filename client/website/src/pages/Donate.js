d9fc6cc5924bc1d7b4d6adc7311fc70101a8d581
Amrit-S <aksingh@ucsd.edu>
2021-11-16 01:48:47 -0800

    updated PayPal Donate button with Sakyadhita's

--- client/website/src/pages/Donate.js
+++ client/website/src/pages/Donate.js
@@ -36,7 +36,7 @@ export default function Donate() {
                         <p> <b> Make a One-Time or Reoccuring Donation </b> </p>
                         <div className="paypal-donate-button-wrapper">
                             <form action="https://www.paypal.com/donate" method="post" target="_blank">
-                                <input type="hidden" name="hosted_button_id" value="8QWLNGYMEFDDU" />
+                                <input type="hidden" name="hosted_button_id" value="78ZSMXL4ZSYJQ" />
                                 <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
                                 <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
                             </form>
