845d486938886273e1dc57bb9470100942e568ca
Navid Boloorian <navidboloorian0@gmail.com>
2022-01-18 02:32:37 -0800

    Wrapper boilerplate

--- client/admin-tool/src/components/Newsletter/NewsletterWrapper.js
+++ client/admin-tool/src/components/Newsletter/NewsletterWrapper.js
@@ -4,7 +4,23 @@ import {Snackbar} from "@material-ui/core";
 import Stepper from "../Stepper";
 import NewsletterItem from "./NewsletterItem";
 
+import "../../css/NewsletterWrapper.css";
+
 export default function NewsletterWrapper() {
+    const [newsletters, setNewsletters] = React.useState([]);
+    const [currentIndex, setIndex] = React.useState(-1);
+    const [isPageLoading, setIsPageLoading] = React.useState(true);
+
+    const [snackbar, handleSnackbar] = React.useState({
+        open: false,
+        message: "",
+    });
+
+    /** Functions */
+    const refreshNewsletters = async () => {
+        setIsPageLoading(true);
+    };
+
     return (
         <NewsletterItem />
     )
