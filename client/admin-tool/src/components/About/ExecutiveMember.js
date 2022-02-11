ac16d437fa54b7d8946c678445b21d314e24cfcf
Navid Boloorian <navidboloorian0@gmail.com>
2022-02-11 01:29:00 -0800

    Fixed add exec bug

--- client/admin-tool/src/components/About/ExecutiveMember.js
+++ client/admin-tool/src/components/About/ExecutiveMember.js
@@ -20,7 +20,7 @@ export default function ExecutiveMember({
                     icon={faTrash}
                     className="trash-icon"
                 />
-                <img src="https://static.remove.bg/remove-bg-web/3a7401e33933f092e5fea5ef460b0cfd79d85fe8/assets/start_remove-c851bdf8d3127a24e2d137a55b1b427378cd17385b01aec6e59d5d4b5f39d2ec.png" />
+                <img src={data.imageLink} />
                 <div className="executive-item-text">
                     <p className="executive-item-name">{data.name}</p>
                     <p className="executive-item-role">{data.position}</p>
