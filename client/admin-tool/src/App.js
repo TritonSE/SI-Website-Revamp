cad2a3cefa7e3cf09051da43c5ee8ea25a918d09
Navid Boloorian <navidboloorian0@gmail.com>
2022-02-23 00:33:24 -0800

    Redirect to login

--- client/admin-tool/src/App.js
+++ client/admin-tool/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import "./App.css";
 
 import { SITE_PAGES } from "./constants/links";
@@ -106,6 +106,7 @@ function App() {
                                     <ExecCommittee />
                                 </PrivateRoute>
                             </Route>
+                            <Redirect path="*" to={SITE_PAGES.ACCOUNTS_LOGIN} />
                         </Switch>
                     </PageLayout>
                 </Route>
